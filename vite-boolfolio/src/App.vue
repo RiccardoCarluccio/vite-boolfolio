7f69bfe283b6a43ef7339134398706c3e89f5c35
RiccardoCarluccio <riccardo.carluccio.93@gmail.com>
2023-10-16 17:47:00 +0200

    do app script

---
+++ vite-boolfolio/src/App.vue
@@ -0,0 +1,36 @@
+<script>
+import axios from "axios";
+
+export default {
+	data() {
+		return {
+			projects: [],
+			pagination: {}
+		};
+	},
+	methods: {
+		fetchData() {
+			axios.get(url ?? "http://127.0.0.1:8000/api/projects")
+				.then((response) => {
+				this.projects = response.data.data;
+				delete response.data.data;
+				this.pagination = response.data;
+      });
+		},
+		getImageUrl(project) {
+			return `http://127.0.0.1:8000/api/projects/${project.image}`;
+		}
+	},
+	mounted() {
+		this.fetchData();
+	}
+};
+</script>
+
+<template>
+  
+</template>
+
+<style scoped>
+
+</style>
